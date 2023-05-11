c31e40351b843a1c4a8489cc75c2a4cf41991958
Joseph Toney <jtoney@thecitybase.com>
2023-05-11 11:08:57 -0500

    PLFM-638 Apply yarn lint --fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import pkg from "./package.json";
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
 
 export default [
   // browser-friendly UMD build
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: {
-      name: "redux-freeform",
+      name: 'redux-freeform',
       file: pkg.browser,
-      format: "umd",
+      format: 'umd',
     },
     plugins: [nodeResolve(), commonjs()],
   },
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
     ],
     plugins: [nodeResolve(), commonjs()],
   },
